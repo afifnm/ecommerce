e75e9ef51d3418084c888271a682687214f1e769
Afif Nuruddin Maisaroh <afifnuruddinmaisaroh@gmail.com>
2022-10-26 16:05:09 +0700

    Update _sidebar.php

--- application/views/layout/_sidebar.php
+++ application/views/layout/_sidebar.php
@@ -8,7 +8,6 @@ $kategori_produk = $this->db->get()->result_array();
     <a href="<?php echo site_url('admin/home');?>" class="app-brand-link">
       <span class="app-brand-text demo  fw-bolder ms-2">e-commerce</span>
     </a>
-
     <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
       <i class="bx bx-chevron-left bx-sm align-middle"></i>
     </a>
